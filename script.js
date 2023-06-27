1de89885efbf2ca3190beb592ca7794cd7b335f8
KilllerGaming <gamerkiller01325@gmail.com>
2023-06-27 23:55:16 +0530

    Changes of Form

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-function previewBeforeUpload(id){
+function previewBeforeUpload1(id){
   document.querySelector("#"+id).addEventListener("change",function(e){
     if(e.target.files.length == 0){
       return;
     }
     let file = e.target.files[0];
     let url = URL.createObjectURL(file);
-    document.querySelector("#"+id+"-preview div").innerText = file.name;
+    // document.querySelector("#"+id+"-preview div").innerText = file.name;
+    document.getElementById("img1").style.display = "block";
     document.querySelector("#"+id+"-preview img").src = url;
+    document.getElementById("one").style.display = "none";
+  });
+}
+function previewBeforeUpload2(id){
+  document.querySelector("#"+id).addEventListener("change",function(e){
+    if(e.target.files.length == 0){
+      return;
+    }
+    let file = e.target.files[0];
+    let url = URL.createObjectURL(file);
+    // document.querySelector("#"+id+"-preview div").innerText = file.name;
+    document.getElementById("img2").style.display = "block";
+    document.querySelector("#"+id+"-preview img").src = url;
+    document.getElementById("two").style.display = "none";
+  });
+}
+function previewBeforeUpload3(id){
+  document.querySelector("#"+id).addEventListener("change",function(e){
+    if(e.target.files.length == 0){
+      return;
+    }
+    let file = e.target.files[0];
+    let url = URL.createObjectURL(file);
+    // document.querySelector("#"+id+"-preview div").innerText = file.name;
+    document.getElementById("img3").style.display = "block";
+    document.querySelector("#"+id+"-preview img").src = url;
+    document.getElementById("three").style.display = "none";
   });
 }
 
-previewBeforeUpload("file-1");
-previewBeforeUpload("file-2");
-previewBeforeUpload("file-3");
+previewBeforeUpload1("file-1");
+previewBeforeUpload2("file-2");
+previewBeforeUpload3("file-3");
